2315c59de4ec153eae457f48df8367a76c3d47e5
Diwakar Pandey <diwakarpandey.2002@gmail.com>
2023-08-04 22:30:21 +0530

    It is a ev car website

---
+++ app.js
@@ -0,0 +1,8 @@
+{/* <script> */}
+const features = document.getElementById("features");
+const footer = document.getElementById("footer");
+features.addEventListener('click',()=>{
+    footer.classList.toggle("footers");
+    features.classList.toggle("clicks")
+})
+// </script>
