419ea69d4b87edb7579b5feaa58b7a227f33915d
Diwakar Pandey <diwakarpandey.2002@gmail.com>
2023-08-04 22:32:20 +0530

    ev cars features
    it is a feature specific site

--- app.js
+++
@@ -1,8 +0,0 @@
-{/* <script> */}
-const features = document.getElementById("features");
-const footer = document.getElementById("footer");
-features.addEventListener('click',()=>{
-    footer.classList.toggle("footers");
-    features.classList.toggle("clicks")
-})
-// </script>
